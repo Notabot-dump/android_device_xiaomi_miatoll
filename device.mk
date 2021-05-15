56fb55e7b904fb15c3bf363775106eabedcd58e0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-15 20:16:18 +0530

    miatoll: Import libnfc-nci.conf from stock
    Change-Id: Ib52912b49c4535cd04896ee811ca4e1eefd1bd3d

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
